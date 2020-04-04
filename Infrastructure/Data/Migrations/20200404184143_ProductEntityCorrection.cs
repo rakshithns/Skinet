36f335e303266ebe41b181ed9f058567e3862342
rakshith <rakshith10csstyle@gmail.com>
2020-04-05 02:27:59 +0530

    Initial Commit

---
+++ Infrastructure/Data/Migrations/20200404184143_ProductEntityCorrection.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Data.Migrations
+{
+    public partial class ProductEntityCorrection : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ProductName",
+                table: "Products",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "ProductName",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
